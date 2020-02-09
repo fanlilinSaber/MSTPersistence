c3b22982546595b4da8e18c486bd4c3d803e8218
范李林 <>
2020-02-09 22:15:05 +0800

    CoreData 使用

---
+++ MSTPersistence/MSTPersistence.h
@@ -0,0 +1,19 @@
+//
+//  MSTPersistence.h
+//  MSTPersistence
+//
+//  Created by Fan Li Lin on 2020/2/9.
+//  Copyright © 2020 fanlilin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MSTPersistence.
+FOUNDATION_EXPORT double MSTPersistenceVersionNumber;
+
+//! Project version string for MSTPersistence.
+FOUNDATION_EXPORT const unsigned char MSTPersistenceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSTPersistence/PublicHeader.h>
+
+
